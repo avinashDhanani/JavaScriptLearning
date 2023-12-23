b4731b9c525ea8e7133959a10ba8bf1429518146
avinashDhanani <avinashdhanani1@gmail.com>
2023-12-23 17:39:35 +0530

    changes in functions file

--- 02_functions.js
+++ 02_functions.js
@@ -1,12 +1,16 @@
 var x = 1;
-a();
-b();
+let y = "Y";
+
 if (true) {
   let x = 1000;
+  y = "YYY";
   console.log("x : ", x);
+  console.log("y : ", y);
 }
 console.log("x : ", x);
-
+console.log("y : ", y);
+a();
+b();
 function a() {
   var x = 10;
   console.log(x);
